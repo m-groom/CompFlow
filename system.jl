9d1c40a18d81f7f9b977aeff15190136d034243e
Michael Groom <michael.groom@sydney.edu.au>
2023-03-25 13:15:13 +1100

    Added functions for converting prim/cons vars

--- system.jl
+++ system.jl
@@ -12,7 +12,7 @@ function Fx(Q, γ)
         ρE = 0.0;
     else
         u = Q[2] / Q[1];
-        p = eos(Q, γ);
+        p = pressure(Q, γ);
         ρE = Q[3];
     end
     flux[1] = ρ * u;          # mass flux
@@ -24,7 +24,7 @@ end
 # Define the flux Jacobian as a function of Q
 function ∂Fx(Q, γ)
     u = Q[2]/Q[1];
-    a = sos(Q, γ);
+    a = speedOfSound(Q, γ);
     J = [0.0 1.0 0.0;
          0.5*(γ-3.0)*u^2 (3.0-γ)*u γ-1.0;
          0.5*(γ-2.0)*u^3-a^2*u/(γ-1.0) 0.5*(3.0-2.0*γ)*u^2+a^2/(γ-1.0) γ*u;];
@@ -34,18 +34,35 @@ end
 # Return the eigenvalues of the flux Jacobian
 function eigval(Q, γ)
     u = Q[2]/Q[1];
-    a = sos(Q, γ);
+    a = speedOfSound(Q, γ);
     return [u-a; u; u+a]
 end
 
 # Return the eigenvectors of the flux Jacobian
 function eigvec(Q, γ)
     ρ = Q[1]; u = Q[2]/Q[1]; ρE = Q[3];
-    p = eos(Q, γ);
-    a = sos(Q, γ);
+    p = pressure(Q, γ);
+    a = speedOfSound(Q, γ);
     H = (ρE + p) / ρ;
     V1 = [1.0; u-a; H-u*a];
     V2 = [1.0; u; 0.5*u^2];
     V3 = [1.0; u+a; H+u*a];
     return [V1 V2 V3]
+end
+
+# Convert conserved variables to primitive variables
+function primitive(Q, γ)
+    ρ = Q[1]; u = Q[2]/Q[1]; ρE = Q[3];
+    p = pressure(Q, γ);
+
+    return [ρ; u; p]
+end
+
+# Convert primitive variables to conserved variables
+function conserved(W, γ)
+    ρ = W[1]; u = W[2]; p = W[3];
+    e = internalEnergy(W, γ);
+    ρE = ρ * (e + 0.5 * u^2);
+
+    return [ρ; ρ*u; ρE]
 end
