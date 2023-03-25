122e2c94cb8e4eaf6c2a82cdb7a9ce9cba2b8d7a
Michael Groom <michael.groom@sydney.edu.au>
2023-03-25 13:13:35 +1100

    Renamed functions

--- equation_of_state.jl
+++ equation_of_state.jl
@@ -1,22 +1,35 @@
 # Functions for equation of state calculations
 
 # Take the vector of conserved variables as input and returns the pressure
-function eos(Q, γ, eosType = "ideal")
+function pressure(Q, γ, eosType = "ideal")
     ρ = Q[1]; u = Q[2]/Q[1]; ρE = Q[3];
     if (eosType == "ideal")
         p = (γ - 1.0)*(ρE - 0.5*ρ*u^2);
     else
         error("EOS type $eosType currently not supported")
+    end
     return p
 end
 
 # Take the vector of conserved variables as input and returns the speed of sound
-function sos(Q, γ = 5/3, eosType = "ideal")
+function speedOfSound(Q, γ = 5/3, eosType = "ideal")
     ρ = Q[1]; u = Q[2]/Q[1]; ρE = Q[3];
     if (eosType == "ideal")
         p = (γ - 1.0)*(ρE - 0.5*ρ*u^2);
         c = sqrt(γ * p / ρ);
     else
         error("EOS type $eosType currently not supported")
+    end
     return c
+end
+
+# Take the vector of primitive variables as input and returns the internal energy
+function internalEnergy(W, γ, eosType = "ideal")
+    ρ = W[1]; u = W[2]; p = W[3];
+    if (eosType == "ideal")
+        e = p / ((γ-1)*ρ);
+    else
+        error("EOS type $eosType currently not supported")
+    end
+    return e
 end
