1b1448e4314bb409dde416e7907a981a93e6b130
Michael Groom <michael.groom@sydney.edu.au>
2023-04-06 10:40:30 +1000

    Added choice of reconstruction variables

--- src/reconstruction.jl
+++ src/reconstruction.jl
@@ -1,40 +1,61 @@
 # Functions for reconstructing the solution at the cell interface
-# TODO: experiment with reconstruction in characteristic variables
 
 # Load functions
 include("system.jl")
 
 # Function for performing reconstruction and returning extrapolated values
 # TODO: generalise boundary conditions
-function reconstruct(Q, γ)
+function reconstruct(Q, γ, recType = "primitive", limiter = "minmod")
     imax = size(Q, 2);
     nVar = size(Q, 1);
-    # Convert to primitive variables
-    W = zeros(nVar, imax);
-    for i = 1:imax
-        W[:, i] = consToPrim(Q[:, i], γ);
+    if (recType == "primitive")
+        # Convert to primitive variables
+        W = zeros(nVar, imax);
+        for i = 1:imax
+            W[:, i] = consToPrim(Q[:, i], γ);
+        end
+    elseif (recType == "conserved")
+        W = Q;
+    else
+        error("Invalid reconstruction type")
     end
     # Piecewise linear reconstruction in space
     ΔW = zeros(nVar, imax);
     for i = 1:imax
-        if (i==1) # Boundary condition
-            ΔW[:,i] = zeros(3,1); 
-        elseif (i==imax) # Boundary condition
-            ΔW[:,i] = zeros(3,1); 
+        if (i==1) 
+            ΔW[:,i] = zeros(3,1); # Dirichlet boundary condition
+        elseif (i==imax) 
+            ΔW[:,i] = zeros(3,1); # Dirichlet boundary condition
         else 
-            # TODO: generalise this to be either first or second-order
-            ΔW[:,i] = minmod.(W[:,i] - W[:,i-1], W[:,i+1] - W[:,i]);
+            if (limiter == "minmod")
+                ΔW[:,i] = minmod.(W[:,i] - W[:,i-1], W[:,i+1] - W[:,i]);
+            elseif (limiter == "superbee")
+                ΔW[:,i] = superbee.(W[:,i] - W[:,i-1], W[:,i+1] - W[:,i]);
+            elseif (limiter == "mc")
+                ΔW[:,i] = mc.(W[:,i] - W[:,i-1], W[:,i+1] - W[:,i]);
+            elseif (limiter == "none") # Central difference
+                ΔW[:,i] = 0.5 * (W[:,i+1] - W[:,i-1]);
+            elseif (limiter == "first")
+                ΔW[:,i] = zeros(3,1); # First order reconstruction
+            else
+                error("Invalid limiter")
+            end
         end
     end
     # Extrapolate to cell boundaries
     WR = W .+ 0.5 * ΔW;
     WL = W .- 0.5 * ΔW;
-    # Convert back to conserved variables
-    QR = zeros(nVar, imax);
-    QL = zeros(nVar, imax);
-    for i = 1:imax
-        QR[:, i] = primToCons(WR[:, i], γ);
-        QL[:, i] = primToCons(WL[:, i], γ);
+    if (recType == "primitive")
+        # Convert back to conserved variables
+        QR = zeros(nVar, imax);
+        QL = zeros(nVar, imax);
+        for i = 1:imax
+            QR[:, i] = primToCons(WR[:, i], γ);
+            QL[:, i] = primToCons(WL[:, i], γ);
+        end
+    elseif (recType == "conserved")
+        QR = WR;
+        QL = WL;
     end
     
     return QR, QL
